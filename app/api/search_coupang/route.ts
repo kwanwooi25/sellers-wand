1b1bab265ed6b9f415c22c0b3bdeabf39977effa
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-05-05 02:51:18 +0900

    set maxDuration for search_coupang

--- app/api/search_coupang/route.ts
+++ app/api/search_coupang/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from 'next/server';
 import { getKeywordSearchResult } from './utils';
 
+export const maxDuration = 50;
+
 export async function GET(req: NextRequest) {
   const keyword = req.nextUrl.searchParams.get('keyword') as string;
   const searchResult = await getKeywordSearchResult(keyword);
