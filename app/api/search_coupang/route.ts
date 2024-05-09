3378d54ab410c7c07387d03f26c0cbf98f627e0c
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-05-10 03:03:15 +0900

    update maxDuration for keyword analysis api

--- app/api/search_coupang/route.ts
+++ app/api/search_coupang/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from 'next/server';
 import { getKeywordSearchResult } from './utils';
 
-export const maxDuration = 50;
+export const maxDuration = 150;
 
 export async function GET(req: NextRequest) {
   const keyword = req.nextUrl.searchParams.get('keyword') as string;
