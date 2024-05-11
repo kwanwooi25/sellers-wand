dffd04299e182b98d2bb97cb80d3c2dbdd3322b2
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-05-11 09:12:19 +0900

    added createdAt and updatedAt columns

---
+++ prisma/migrations/20240511001128_added_created_at_and_updated_at_columns/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Expense` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Inventory` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Purchase` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Sales` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Expense" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Inventory" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Purchase" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Sales" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
