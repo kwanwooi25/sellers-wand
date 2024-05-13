8fabba79227ea1a60bc9622205a82de39137c7a3
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-05-13 13:22:25 +0900

    make withAuth hoc client component

--- lib/auth/hoc.tsx
+++ lib/auth/hoc.tsx
@@ -1,14 +1,18 @@
+'use client';
+
 import AuthRequired from '@/components/AuthRequired';
 import { auth } from '.';
 import NeedUserUpgrade from '@/components/NeedUserUpgrade';
+import { useSession } from 'next-auth/react';
 
 export function withAuth(Component: () => JSX.Element | Promise<JSX.Element>) {
   return async function WithAuth() {
-    const session = await auth();
+    // const session = await auth();
+    const session = useSession();
 
-    if (!session) {
+    if (!session.data?.user) {
       return <AuthRequired />;
-    } else if (session?.user?.grade === 'GUEST') {
+    } else if (session.data.user?.grade === 'GUEST') {
       return <NeedUserUpgrade />;
     }
 
